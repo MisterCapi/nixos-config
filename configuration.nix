06288566c9e873cdc544a88831393de1f17e0097
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-13 00:28:45 +0200

    Set numlock default ON

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,9 @@
   hardware.graphics.enable = true;
 
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.settings = {
+    General.Numlock = "on";
+  };
   services.desktopManager.plasma6.enable = true;
   services.xserver.xkb.layout = "pl";
 
