ce5642343044eef87851bf31c9b245e42671003c
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-17 02:20:47 +0200

    Set disko version in lock

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,12 +8,10 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "ahci" "xhci_pci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ahci" "xhci_pci" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
-  # fileSystems and swap are managed by disko (install/disk-config.nix)
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
