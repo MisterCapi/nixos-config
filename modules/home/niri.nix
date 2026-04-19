c03f7f233df232828e549029a20bc5b66480087b
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-19 19:58:47 +0200

    Add missing keybings

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -31,29 +31,29 @@
 	# exit
         "Mod+Shift+E".action.quit = [];
 
-        "Mod+H".action = focus-column-left;
-        "Mod+L".action = focus-column-right;
+        "Mod+H".action.focus-column-left = {};
+        "Mod+L".action.focus-column-right = {};
   
-        "Mod+Shift+H".action = move-column-left;
-        "Mod+Shift+L".action = move-column-right;
+        "Mod+Shift+H".action.move-column-left = {};
+        "Mod+Shift+L".action.move-column-right = {};
   
-        "Mod+Shift+Page-Down".action = move-column-to-workspace-down;
-        "Mod+Shift+Page-Up".action = move-column-to-workspace-up;
+        "Mod+Shift+Page-Down".action.move-column-to-workspace-down = {};
+        "Mod+Shift+Page-Up".action.move-column-to-workspace-up = {};
   
-        "Mod+Page-Down".action = switch-workspace-down;
-        "Mod+Page-Up".action = switch-workspace-up;
+        "Mod+Page-Down".action.switch-workspace-down = {};
+        "Mod+Page-Up".action.switch-workspace-up = {};
   
-        "Mod+R".action = switch-preset-column-widths;
-        "Mod+M".action = maximize-column;
+        "Mod+R".action.switch-preset-column-widths = {};
+        "Mod+M".action.maximize-column = {};
   
-        "Mod+Comma".action = consume-or-expel-window-left;
-        "Mod+Period".action = consume-or-expel-window-right;
+        "Mod+Comma".action.consume-or-expel-window-left = {};
+        "Mod+Period".action.consume-or-expel-window-right = {};
   
-        "Mod+V".action = toggle-window-floating;
-        "Mod+F".action = toggle-focus-follows-mouse; # albo switch-focus-between-floating-and-tiling
+        "Mod+V".action.toggle-window-floating = {};
+        "Mod+F".action.toggle-focus-follows-mouse = {};
   
-        "Mod+Slash".action = show-hotkey-overlay;
-        "Mod+O".action = toggle-overview;
+        "Mod+Slash".action.show-hotkey-overlay = {};
+        "Mod+O".action.toggle-overview = {};
       };
 
       layout = {
