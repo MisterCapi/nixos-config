7170914042865646f6dc6090433d87a2b40f57a3
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-26 21:37:53 +0200

    Autoimport shared modules

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,4 +1,4 @@
-{ self, inputs, ... }:
+{ self, inputs, lib, ... }:
 {
   imports = [ inputs.home-manager.flakeModules.default ];
 
@@ -11,15 +11,12 @@
       extraSpecialArgs = { inherit inputs; }; # dodatkowe args dla home modules
 
       users.${config.my.username} = {
-        imports = [
           # === Globalne moduły - aktywne na każdym hoście ===
           # np. self.homeModules.git, self.homeModules.neovim
-          # host-specific rzeczy idą do hosts/<nazwa>/default.nix
-	  self.homeModules.niri
-	  self.homeModules.eza
-	  self.homeModules.bash 
-	  # replace it later with better shell
-        ];
+          # host-specific rzeczy idą do hosts/<nazwa>/default.nix i powinny być nazywane z prefixem "_"
+	  imports = builtins.attrValues (
+            lib.filterAttrs (name: _: !lib.hasPrefix "_" name) self.homeModules
+	  );
         home.username = config.my.username;
         home.homeDirectory = "/home/${config.my.username}";
         home.stateVersion = config.my.stateVersion;
