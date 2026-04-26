4ead4d1ec237d5dc61e06ab12a147119d84e968a
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-26 22:39:18 +0200

    Switch to Nushell

--- modules/home/nushell.nix
+++ modules/home/nushell.nix
@@ -4,6 +4,14 @@
     programs.nushell = {
       enable = true;
 
+      shellAliases = {
+        # eza
+        tree = "eza --tree --group-directories-first --icons=auto";
+
+	# nixos
+	nixswitch = "sudo nixos-rebuild switch --flake";
+	nixtest = "sudo nixos-rebuild test --flake";
+      };
     };
   };
 }
