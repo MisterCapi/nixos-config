bc806e2cafb9636028aa9b49bc077be1017dd900
MisterCapi <github.everest@kacperstefaniak.com>
2026-04-29 08:30:02 +0200

    Add manual chrome theme (stylix won't work)

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,28 +1,32 @@
-{ inputs, config, ... }:
-let
-  profileDir = "${config.home.homeDirectory}/.mozilla/firefox/default";
-in
+# modules/home/firefox.nix
+{ inputs, ... }:
 {
-  programs.firefox = {
-    enable = true;
-    profiles.default = {
-      settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  flake.homeModules.firefox = { config, ... }:
+  let
+    profileDir = "${config.home.homeDirectory}/.mozilla/firefox/default";
+  in
+  {
+    programs.firefox = {
+      enable = true;
+      profiles.default = {
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        };
       };
     };
-  };
 
-  stylix.targets.firefox.enable = false;
+    stylix.targets.firefox.enable = false;
 
-  home.file = let
-    rp = inputs.rosepine-firefox;
-    chrome = "${profileDir}/chrome";
-  in {
-    "${chrome}/userChrome.css".source  = "${rp}/dist/userChrome.css";
-    "${chrome}/userColors.css".source  = "${rp}/dist/userColors.css";
-    "${chrome}/userContent.css".source = "${rp}/dist/userContent.css";
-    "${chrome}/add.svg".source         = "${rp}/dist/add.svg";
-    "${chrome}/left-arrow.svg".source  = "${rp}/dist/left-arrow.svg";
-    "${chrome}/right-arrow.svg".source = "${rp}/dist/right-arrow.svg";
+    home.file = let
+      rp = inputs.rosepine-firefox;
+      chrome = "${profileDir}/chrome";
+    in {
+      "${chrome}/userChrome.css".source  = "${rp}/dist/userChrome.css";
+      "${chrome}/userColors.css".source  = "${rp}/dist/userColors.css";
+      "${chrome}/userContent.css".source = "${rp}/dist/userContent.css";
+      "${chrome}/add.svg".source         = "${rp}/dist/add.svg";
+      "${chrome}/left-arrow.svg".source  = "${rp}/dist/left-arrow.svg";
+      "${chrome}/right-arrow.svg".source = "${rp}/dist/right-arrow.svg";
+    };
   };
 }
